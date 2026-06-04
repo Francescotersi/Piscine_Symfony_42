f0e012a19fa631cdd3bd33ece455ffd9d9ed540d
Francesco <francescotersillo@gmail.com>
2026-06-04 13:54:31 +0200

    ex02 module00Oop done

---
+++ module_00_Oop/ex02/TemplateEngine.php
@@ -0,0 +1,46 @@
+<?php
+
+include('./HotBeverage.php');
+
+class TemplateEngine {
+
+    public function createFile(HotBeverage $text) {
+        $templateName = "template.html";
+
+        if (!file_exists($templateName)) {
+            throw new Exception("Il file template '$templateName' non esiste.");
+        }
+        
+        $htmlContent = file_get_contents($templateName);
+
+        $reflection = new ReflectionClass($text);
+
+        $className = $reflection->getShortName();
+        $fileName = $className;
+        $properties = [];
+        $currentReflection = $reflection;
+
+        while ($currentReflection != null) {
+            foreach ($currentReflection->getProperties() as $propriety) {
+                $properties[$propriety->getName()] = $propriety;
+            }
+            $currentReflection = $currentReflection->getParentClass();
+        }
+
+        foreach ($properties as $property) {
+            $propertyName = $property->getName();
+            $getterName = 'get' . ucfirst($propertyName);
+            if (method_exists($text, $getterName)) {
+                $value = $text->$getterName();
+                $htmlContent = str_replace("{" . $propertyName . "}", $value, $htmlContent);
+            }
+        }
+
+        if (file_put_contents($fileName . ".html", $htmlContent)) {
+        	echo "File created successfully: mendeleiev.html\n";
+        } 
+        else {
+        	echo "Error creating file: mendeleiev.html\n";
+        }
+    }
+}
