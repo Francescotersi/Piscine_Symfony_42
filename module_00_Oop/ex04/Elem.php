39b25167e74a3d81224daeb82bb977cc84d97c11
Francescotersi <francescotersillo@gmail.com>
2026-06-05 11:45:53 +0200

    Module00Oop ex04 done

--- module_00_Oop/ex04/Elem.php
+++ module_00_Oop/ex04/Elem.php
@@ -3,21 +3,29 @@
 class Elem {
     private string $content = "";
     private string $element = "";
+    private array $attributes = [];
+    private array $supportedTags = [
+        "meta", "img", "hr", "br", "html", "head", "body", "title",
+        "h1", "h2", "h3", "h4", "h5", "h6",
+        "p", "span", "div",
+        "table", "tr", "th", "td",
+        "ul", "ol", "li"
+    ];
     private array $voidTags = ["meta", "img", "hr", "br"];
 
-    public function __construct(string $element, string $content = "") {
+    public function __construct(string $element, string $content = "", array $attributes = []) {
         if (!$element) {
             echo "Error: error invalid arguments";
             return;
         }
-        $supportedTags = ["meta","img","hr","br","html","head","body","title","h1","h2","h3","h4","h5","h6","p","span","div"];
         $this->content = $content;
+        $this->attributes = $attributes;
 
-        if (in_array($element, $supportedTags)) {
+        if (in_array($element, $this->supportedTags)) {
             $this->element = $element;
         }
         else {
-            echo "Error: error invalid tag entered";
+            throw new MyException();
             return;
         }
     }
@@ -34,13 +42,24 @@ class Elem {
         }
     }
 
+    public function getAttributesHtml() {
+        $attributesHtml = "";
+        foreach ($this->attributes as $key => $value) {
+            $attributesHtml .= " " . $key . '="' . $value . '"';
+        }
+        return $attributesHtml;
+    }
+
     public function getHTML() {
         if (in_array($this->element, $this->voidTags)) {
-            return "<" . $this->element . ">\n\t";
+            return "<" . $this->element . $this->getAttributesHtml() .">\n\t";
         }
+        // else {
+        //     return "<" . $this->element . ">\n\t";
+        // }
 
         if ($this->content === "") {
-            return "<" . $this->element . "></" . $this->element . ">\n";
+            return "<" . $this->element . $this->getAttributesHtml() . "></" . $this->element . ">\n";
         }
 
         // per indentazione bellina
@@ -52,6 +71,6 @@ class Elem {
                 $indentedContent .= "\t" . $line . "\n";
             }
         }
-        return "<" . $this->element . ">\n" . $indentedContent . "</" . $this->element . ">\n";
+        return "<" . $this->element . $this->getAttributesHtml() . ">\n" . $indentedContent . "</" . $this->element . ">\n";
     }
 }
--- module_00_Oop/ex04/index.php
+++ module_00_Oop/ex04/index.php
@@ -2,16 +2,17 @@
 
 include('./Elem.php');
 include('./TemplateEngine.php');
+include('./MyException.php');
 
 try {
     $elem = new Elem('html');
     $body = new Elem('body');
-    $body->pushElement(new Elem('p', 'Lorem ipsum'));
+    $body->pushElement(new Elem('p', 'Lorem ipsum', ['class' => 'text-muted']));
     $elem->pushElement($body);
-
-    $template = new TemplateEngine($elem);
-    $template->createFile("meow");
+    $templateEngine = new TemplateEngine($elem);
+    $templateEngine->createFile("Donald");
+    $elem = new Elem('undefined');
 }
 catch (Exception $e) {
-    echo ''. $e->getMessage() .'';
+    echo '' . $e->getMessage() . '';
 }
