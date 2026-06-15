b5ebc2502197a59d9d61cd3b13792af9770f3f12
Francesco <francescotersillo@gmail.com>
2026-06-15 16:46:02 +0200

    module01BaseSymfony ex01 done

--- Module_01_BaseSymfony/d04/src/Controller/ex01Controller.php
+++ Module_01_BaseSymfony/d04/src/Controller/ex01Controller.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 
-class ex01Controller {
+class ex01Controller extends AbstractController
+{
+    private const PAGES = ['wall-e', 'up', 'madagascar'];
 
-    #[Route('/e01/firstpage', name:'ex01_firstpage')]
+
+
+    #[Route('/e01', name:'ex01_main')]
     public function index(): Response
     {
-        return new Response(
-            file_get_contents('./base.html.twig'),
-        );
+        return $this->render('ex01/index.html.twig', [
+            'articles' => self::PAGES,
+        ]);
+    }
+
+    #[Route('/e01/{article}', name:'ex01_article')]
+    public function showArticle(string $article): Response
+    {
+        if (!in_array($article, self::PAGES)) {
+            return $this->render("ex01/index.html.twig", [
+                'articles' => self::PAGES,
+                'error_message' => "The Chosen films hasn`t been made yet. Please choose one that exists:"
+            ]);
+        }
+        else {
+            return $this->render("ex01/{$article}.html.twig", [
+                'title' =>ucfirst($article),
+            ]);
+        }
     }
 }
