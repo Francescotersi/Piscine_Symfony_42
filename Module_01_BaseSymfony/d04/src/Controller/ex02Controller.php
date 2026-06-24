24955f0af7819b2019d53d5391f44f41484c4dd9
Francesco Tersillo <francescotersillo@gmail.com>
2026-06-24 16:18:50 +0200

    reverted changes from last commit in Module01BaseSymfony ex01

---
+++ Module_01_BaseSymfony/d04/src/Controller/ex02Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ex02Controller extends AbstractController {
+    
+}
