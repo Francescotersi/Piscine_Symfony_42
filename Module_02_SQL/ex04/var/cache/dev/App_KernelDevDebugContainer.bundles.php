d47f8f52140e9f058ce5659ec9494134ed7d0947
Francesco Tersillo <francescotersillo@gmail.com>
2026-07-19 16:05:24 +0200

    Started ex04 Module02SQL

---
+++ Module_02_SQL/ex04/var/cache/dev/App_KernelDevDebugContainer.bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'ServicesBundle' => new \Symfony\Component\DependencyInjection\Kernel\ServicesBundle(),
+    'ConsoleBundle' => new \Symfony\Component\Console\ConsoleBundle(),
+    'FrameworkBundle' => new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+    'TwigBundle' => new \Symfony\Bundle\TwigBundle\TwigBundle(),
+];
